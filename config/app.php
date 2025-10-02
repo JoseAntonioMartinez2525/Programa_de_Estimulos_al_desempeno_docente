b12541f0b0ab54923efad72d6abc62da98edd7ed
JoseAntonioMartinez2525 <bubbleteahyung@gmail.com>
2025-10-02 09:14:59 -0700

    Update dependencies

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+            'PDF' => Barryvdh\Snappy\Facades\SnappyPdf::class,
+            'Dompdf' => Barryvdh\DomPDF\Facade\Pdf::class,
     ])->toArray(),
 
 ];
